2b6fd303fab1822c7f065361d3f04e3f37eda7a1
Mohamed AbouBakr <itsmoaboubakr@outlook.com>
2023-09-17 15:20:28 +0300

    Create LBIT_math.h

---
+++ 1-LIB/LBIT_math.h
@@ -0,0 +1,15 @@
+/************************************/
+/*   Author:    mBravo18            */
+/*   SWC:       BIT MATH            */
+/*   Layer:     LIB                 */
+/************************************/
+
+#ifndef _LBIT_MATH_H_
+#define _LBIT_MATH_H_
+
+#define 	SET_BIT(reg,bit) 		reg |=  (1<<bit)
+#define 	CLR_BIT(reg,bit) 		reg &= ~(1<<bit)
+#define 	TOG_BIT(reg,bit) 		reg ^=  (1<<bit)
+#define 	GET_BIT(reg,bit) 		((reg>>bit) & 1)
+
+#endif
